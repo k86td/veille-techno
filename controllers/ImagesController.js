ba042db43268adaa128eaba549c78db3040599f2
k86td <tristanlepine14@gmail.com>
2023-04-05 22:35:28 -0400

    Removed everything, moving on to another class

--- controllers/ImagesController.js
+++
@@ -1,7 +0,0 @@
-const ImagesRepository = require('../models/imagesRepository');
-module.exports =
-    class ImagesController extends require('./Controller') {
-        constructor(HttpContext) {
-            super(HttpContext, new ImagesRepository(), false, false); // todo pas d'acces anonyme
-        }
-    }
--- serverVariables.js
+++
@@ -1,5 +0,0 @@
-exports.get = (variableName) => {
-    var propertiesReader = require('properties-reader');
-    var properties = propertiesReader('./serverVariables.ini');
-    return properties.get(variableName);
-}
